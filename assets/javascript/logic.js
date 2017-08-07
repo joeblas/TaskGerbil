41fc0c295c77419d9b9e756934a0b2e816baf909
Tyler Negro <tnegro@gmail.com>
2017-08-06 18:54:15 -0600

    login and logout system working with single page. Need to work on register function

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -17,10 +17,26 @@
     var email;
     var password;
 
-
     
 
-    //sign up and login user on click
+    //if user is logged in handling
+    auth.onAuthStateChanged(function(user){
+        if(user){
+            console.log(user)
+            $(".login-cover").hide();
+            $(".login-card").hide();
+            
+
+        } else {
+            $(".login-cover").show();
+            $(".login-card").show();
+            
+        } 
+    });
+    
+
+
+    //sign up and login user on click with Firebase
     $('.login-signup').on('click', function(event){
         event.preventDefault();
         var result = event.currentTarget.id
@@ -34,9 +50,7 @@
             }).catch(function(error){
                 var errCode = error.code;
                 var errMessage = error.message;
-                console.log(errCode);
-                console.log(errMessage);
-                alert(errMessage)
+                $('.card-block').prepend('<p class="alert alert-danger">'+errMessage+'</p>')
             }) 
         }
         else if (result === 'login-btn') {
@@ -45,7 +59,7 @@
             }).catch(function(error){
                 var errCode = error.code;
                 var errMessage = error.message;
-                alert(errMessage)
+                $('.card-block').prepend('<p class="alert alert-danger">'+errMessage+'</p>')
             })
         }
         $('form').each(function(){
@@ -53,29 +67,27 @@
             })  
     })
 
+
     //handles the signout of the user
-    $('button').on('click', function(event){
+    $('#user-signout').on('click', function(event){
         event.preventDefault();
         console.log(event)
+        $('.alert').remove(); //removes any alerts if user logs out and goes to login screen
         auth.signOut().catch(function(error){
             var errCode = error.code;
             var errMessage = error.message;
-            alert(errMessage)
+            //create error
         })
     })
-    
-    //if user signed in, load dashboard else load login.html
-    firebase.auth().onAuthStateChanged(function(user){
-        console.log(user)
-        if(user) {
-            $('body').empty();
-            $('body').load('dashboard.html')
-        } 
-        // else {
-        //     $('body').empty();
-        //     $('body').load('login.html')
-        // }
+
+    //shows user registration card if register button is pressed on login card
+    $('#user-register').on('click', function(){
+        $('.login-card').hide();
+        $('.register-card').show();
     });
+    
+
+    
 
     
 
